b1ce89e6a6c1e91d6a2bf0ebcae540f7302ff90a
Boaz Yoo <blazeryoo1921@gmail.com>
2021-05-14 05:40:01 -0400

    Update image files

--- auto_crd.py
+++ auto_crd.py
@@ -1,6 +1,6 @@
 import pyautogui as pag
 
 while True:
-    if pag.locateOnScreen('./images/crd_prompt.png') != None:
-        x, y = prompt_center = pag.locateCenterOnScreen('./images/crd_continue.png', confidence=0.8)
+    if pag.locateOnScreen('./images/prompt.png', confidence=0.8) != None:
+        x, y = pag.locateCenterOnScreen('./images/continue.png', confidence=0.8)
         pag.click(x=x, y=y)
