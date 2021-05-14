6d30613bdbd7476b03335b857fe19dba95a3d90b
Boaz Yoo <blazeryoo1921@gmail.com>
2021-05-14 05:45:38 -0400

    Update auto_crd.py

--- auto_crd.py
+++ auto_crd.py
@@ -1,6 +1,8 @@
 import pyautogui as pag
 
 while True:
-    if pag.locateOnScreen('./images/prompt.png', confidence=0.8) != None:
-        x, y = pag.locateCenterOnScreen('./images/continue.png', confidence=0.8)
-        pag.click(x=x, y=y)
+    # ensure both prompt and continue button are present
+    prompt = pag.locateOnScreen('./images/prompt.png', confidence=0.8)
+    continue_button = pag.locateCenterOnScreen('./images/continue.png', confidence=0.8)
+    if prompt != None and continue_button != None:
+            pag.click(x=continue_button[0], y=continue_button[1])
