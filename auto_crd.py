cc146605b22e4219001cf763d62c89fc01058549
Boaz Yoo <blazeryoo1921@gmail.com>
2021-05-14 05:27:52 -0400

    Update auto_crd.py

--- auto_crd.py
+++ auto_crd.py
@@ -4,4 +4,3 @@ while True:
     if pag.locateOnScreen('./images/crd_prompt.png') != None:
         x, y = prompt_center = pag.locateCenterOnScreen('./images/crd_continue.png', confidence=0.8)
         pag.click(x=x, y=y)
-        break
