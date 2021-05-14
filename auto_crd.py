c8481f9cbd637f93002dc91ff8ad7c6fa3aacfb8
Boaz Yoo <blazeryoo1921@gmail.com>
2021-05-14 04:51:38 -0400

    add files

---
+++ auto_crd.py
@@ -0,0 +1,7 @@
+import pyautogui as pag
+
+while True:
+    if pag.locateOnScreen('./images/crd_prompt.png') != None:
+        x, y = prompt_center = pag.locateCenterOnScreen('./images/crd_continue.png', confidence=0.8)
+        pag.click(x=x, y=y)
+        break
